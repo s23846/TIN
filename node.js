56a6d90631c12801435b71a596b9b6ba58ec1c4b
s23846 <75524166+s23846@users.noreply.github.com>
2023-05-06 11:50:26 +0200

    Add files via upload

--- node.js
+++ node.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-// Funkcje zwracające różne stringi
+
 function home() {
   return 'Witaj na stronie głównej!';
 }
@@ -13,7 +13,7 @@ function contact() {
   return 'Skontaktuj się z nami';
 }
 
-// Tworzenie serwera HTTP
+
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -30,7 +30,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-// Uruchomienie serwera na porcie 3000
+
 server.listen(3000, () => {
   console.log('Serwer uruchomiony na porcie 3000');
 });
